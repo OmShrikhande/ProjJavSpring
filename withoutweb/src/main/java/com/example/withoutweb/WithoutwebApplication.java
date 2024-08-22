eae804fd9a7c2d184afe5f9410db09c361f79a02
jis24 <165779138+orscoder@users.noreply.github.com>
2024-08-22 23:15:48 +0530

    new proj added

---
+++ withoutweb/src/main/java/com/example/withoutweb/WithoutwebApplication.java
@@ -0,0 +1,13 @@
+package com.example.withoutweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithoutwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithoutwebApplication.class, args);
+	}
+
+}

3d9eaa32349c44ec54a624c538b5b922f3686614
jis24 <165779138+orscoder@users.noreply.github.com>
2024-08-24 22:43:10 +0530

    IOC container
    in this commit we learn about the ioc container and the component annotation and how to make spring manage the object of the class crated by us.

---
+++ withoutweb/src/test/java/com/example/withoutweb/Dev.java
@@ -0,0 +1,10 @@
+package com.example.withoutweb;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+    public void build(){
+        System.out.println("working on Awesome project");
+    }
+}
--- withoutweb/src/test/java/com/example/withoutweb/WithoutwebApplicationTests.java
+++ withoutweb/src/test/java/com/example/withoutweb/WithoutwebApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.withoutweb;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
-@SpringBootTest
+@SpringBootApplication
 class WithoutwebApplicationTests {
 
-	@Test
-	void contextLoads() {
+	public static void main(String[] args){
+
+		ApplicationContext context = SpringApplication.run(WithoutwebApplicationTests.class, args);
+
+		Dev obj = context.getBean(Dev.class);
+
+		obj.build();
+
+
 	}
 
 }

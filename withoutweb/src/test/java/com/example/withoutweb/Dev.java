b695463be2abc9db7a37034ffbea1f704c33cc88
jis24 <165779138+orscoder@users.noreply.github.com>
2024-08-30 17:27:28 +0530

    constructor and setter injection
    the construtor and the setter injection is applied for the autowiring. this only in dev an laptop is remaining the same.

--- withoutweb/src/test/java/com/example/withoutweb/Dev.java
+++ withoutweb/src/test/java/com/example/withoutweb/Dev.java
@@ -5,8 +5,20 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Dev {
-    @Autowired
+   // @Autowired //this is field injection
     private Laptop laptop;
+
+
+    //the constructor injection
+//        public Dev(Laptop laptop){
+//            this.laptop = laptop;
+//        }
+
+    // the setter injection
+    @Autowired
+    public void setLaptop(Laptop laptop){
+        this.laptop= laptop;
+    }
     public void build(){
        laptop.compile();
         System.out.println("working on Awesome project");

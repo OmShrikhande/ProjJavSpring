699edcdcaf7d16511d83df4dbc6ef74ef0fc9e93
jis24 <165779138+orscoder@users.noreply.github.com>
2024-08-30 17:08:40 +0530

    Autowiring

--- withoutweb/src/test/java/com/example/withoutweb/Dev.java
+++ withoutweb/src/test/java/com/example/withoutweb/Dev.java
@@ -1,10 +1,14 @@
 package com.example.withoutweb;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Dev {
+    @Autowired
+    private Laptop laptop;
     public void build(){
+       laptop.compile();
         System.out.println("working on Awesome project");
     }
 }

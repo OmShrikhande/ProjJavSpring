95c93848f308e7cbc0f553d984a500ba52782cca
jis24 <165779138+orscoder@users.noreply.github.com>
2024-09-05 06:27:51 +0530

    setter injection applied

--- simpleWebApp/src/main/java/com/devlopers/simpleWebApp/Dev.java
+++ simpleWebApp/src/main/java/com/devlopers/simpleWebApp/Dev.java
@@ -1,9 +1,16 @@
 package com.devlopers.simpleWebApp;
 
 public class Dev {
+	private int age;
 
     public void build(){
         System.out.println("working on Awesome project");
+    }
+    public int getAge() {
+    	return age;
+    }
+    public void setAge(int age) {
+    	this.age = age;
     }
 
 }
